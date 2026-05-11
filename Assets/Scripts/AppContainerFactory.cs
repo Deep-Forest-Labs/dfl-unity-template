fb81b9cba93b6c4d5dd5ae16f47e066ec97483ca
Rob Luckfield <rob.luckfield@inkgames.com>
2026-05-11 16:52:38 -0500

    add audio engine to core template

--- Assets/Scripts/AppContainerFactory.cs
+++ Assets/Scripts/AppContainerFactory.cs
@@ -1,15 +1,19 @@
 #nullable enable
-using Cysharp.Threading.Tasks;
 using DeepForestLabs;
+using DeepForestLabs.Audio;
 using DeepForestLabs.Factories;
 using UnityEngine;
 
 namespace GameName.Factories {
     public sealed class AppContainerFactory : ContainerBuilderFactory
     {
+        [SerializeField] private AudioMixerConfig _audioMixerConfig = default!;
+        [SerializeField] private SoundCatalog? _soundCatalog;
+
         public override IContainerBuilder AddToBuilder(IContainerBuilder builder)
         {
             return builder
+                .AddAudioService(_audioMixerConfig, _soundCatalog)
                 .AddTransient<AppState>();
         }
     }
