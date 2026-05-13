9bf36574f49788b86b50366ffd57a3f7a550db44
Rob Luckfield <rob.luckfield@inkgames.com>
2026-05-13 15:40:15 -0500

    setup for record struct usage

---
+++ Assets/Scripts/IsExternalInit.cs
@@ -0,0 +1,11 @@
+#nullable enable
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit
+    {
+    }
+}
+#nullable disable
